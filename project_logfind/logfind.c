e49e17a71578fdccd7b03c59e3f7c6e73507cc24
Patrick-Cullinane <Patrick.Cullinane@ibm.com>
2020-04-25 11:54:32 -0400

    prints out the kw by line for each file in the cur directory

--- project_logfind/logfind.c
+++ project_logfind/logfind.c
@@ -1,101 +1,49 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h> // explicit def of malloc
-#include "dbg.h"
-#include <errno.h>
+#include <dirent.h>
 
+void FindWord(char *word , char *file) {
+    char *line = NULL;
+    size_t n = 0;
+    FILE *f = fopen(file, "r");
+    int ctr = 0;
 
-// @TODO
-// get the line that the text falls on 
-// iterate through files and return filename that contains kw
-// take in multiple words
-// add -o argument
-// look into glob
-// return matching lines
+    while (getline(&line, &n, f) != -1)
+    {
+    	ctr++;
 
-// int argc, char * argv[]
-
-void die(const char *message)
-{
-	if (errno) {
-		perror(message);
-	} else {
-		printf("ERROR: %s\n", message);
-	}
-
-	exit(1);
-}
-
-int count_lines(FILE *file)
-{
-	char z;
-	int ctr = 0;
-
-	for (z = getc(file); z != EOF; z = getc(file))
-			if (z == '\n')
-				ctr += 1;
-
-	return ctr;	
+        if (strstr(line , word ) != NULL)
+        {
+             printf("'%s' appears on line %d\n", word, ctr);
+        }
+    }
+    
+    fclose(f);
+    free(line);
 }
 
 
+int main(void) 
+{ 
 
-int main(int argc, char *argv[])
-{
+  DIR *p;
+  struct dirent *pp;     
+  p = opendir ("./");
 
-	if (argc < 2)
-		die("enter a word to search for");
+  if (p != NULL)
+  {
+    while ((pp = readdir (p))!=NULL) {
+      int length = strlen(pp->d_name);
+      if (strncmp(pp->d_name + length - 4, ".txt", 4) == 0) {
+      	  puts(pp->d_name);
+          FindWord("text", pp->d_name);
+      }
+    }
 
-	long length; // this can be long but set to int for now
-	char *buffer = 0;
-	char input[100];
-	char *word =  strcpy(input, argv[1]);
-
-
-	FILE *file;
-	file = fopen("test.txt", "rb"); // add filename to this
-	int lines = count_lines(file);
-
-	if(file) {
-		fseek(file, 0, SEEK_END); // Get the end of the file so that ftell can get the length
-		length = ftell(file); // determine the length and store as var
-		fseek(file, 0, SEEK_SET); // Get back to the begin of file
-		buffer = malloc(length); // create buffer n length
-
-		if(buffer) {
-			fread(buffer, 1, length, file);
-		}
-	}
-
-	for (i = 1; i < lines; i++)
-	{
-		if(buffer) {
-			if(strstr(buffer, word) != NULL) {
-				printf("text \n");
-				printf("the lines in the file are: %d\n", lines); 
-			} else {
-				printf("no word \n");
-			}
-		}
-
-
-	}
-
-
-	fclose(file);
-
-	return 0;
-	
-}
-
-
-
-
-
-
-
-
-
+    (void) closedir (p);
+  } 
+} 
 
 
 
